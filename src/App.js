6637fccf4250b84a93574f66a7753c5f0c95969c
meetgojiya98 <65299677+meetgojiya98@users.noreply.github.com>
2025-06-30 10:46:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -655,7 +655,7 @@ export default function App() {
 
       {/* Floating Resume Download Button */}
       <a
-        href="https://drive.google.com/file/d/1_4e5xp2ZGU1E4WiWoKyaRWIWRe7z2gVI/view?usp=sharing" // update with your resume file location
+        href="https://drive.google.com/file/d/1d8C33RiAOEV_1q_QDPrWC0uk-i8J4kqO/view?usp=sharing" // update with your resume file location
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-20 right-6 z-50 text-white px-5 py-3 rounded-full shadow-lg transition flex items-center space-x-2 select-none"
